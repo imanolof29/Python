5433b8de10a2957cb2271d52875f2ab09ec102aa
Imanol Ortiz <57615744+imanolof29@users.noreply.github.com>
2020-09-21 17:04:50 +0200

    Delete product.py

--- product.py
+++
@@ -1,9 +0,0 @@
-class Product:
-    def __init__(self, code, name, price, weight):
-        self.code = code
-        self.name = name
-        self.price = price
-        self.weight = weight
-
-    def get_last_code(self):
-        return self.code +1
